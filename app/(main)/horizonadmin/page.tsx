a5d814c3fbc7e4d264edd67c7087109f00283593
Ashish Singh Negi <ashishamonegi@gmail.com>
2025-10-07 13:06:15 +0530

    fix bug

--- app/(main)/horizonadmin/page.tsx
+++ app/(main)/horizonadmin/page.tsx
@@ -24,7 +24,7 @@ const HorizonAdminPage = () => {
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [isAuth, setIsAuth] = useState(true);
+  const [isAuth, setIsAuth] = useState(false);
   const [error, setError] = useState("");
 
   const getAllRSVPs = async () => {

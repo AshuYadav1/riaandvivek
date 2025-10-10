75dbf5f0f1022608b133d8444ec6a0cef0c97961
Ashish Singh Negi <ashishamonegi@gmail.com>
2025-10-10 13:57:35 +0530

    refactor rsvp

--- app/(main)/rsvp2/page.tsx
+++ app/(main)/rsvp2/page.tsx
@@ -222,7 +222,7 @@ export default function RSVPPage2() {
                 className="mt-6"
               >
                 <p className="text-sm font-medium text-white mb-3 font-alice">
-                  Attending the wedding* (Excluding Your Information)
+                  Attending the wedding*
                 </p>
                 <div className="flex flex-col sm:flex-row gap-3 sm:gap-4">
                   {["Yes", "No", "Maybe"].map((option) => (
@@ -255,7 +255,8 @@ export default function RSVPPage2() {
                 }}
               >
                 <label className="block text-white font-alice mb-2">
-                  Number of Family Members Attending*
+                  Number of Family Members Attending* (Excluding Your
+                  Information)
                 </label>
                 <input
                   type="number"

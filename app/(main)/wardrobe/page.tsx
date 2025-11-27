fe48028abd7930f087a8c85011d129ac599f8bb8
Ashish Singh Negi <ashishamonegi@gmail.com>
2025-11-27 13:43:24 +0530

    fix

--- app/(main)/wardrobe/page.tsx
+++ app/(main)/wardrobe/page.tsx
@@ -5,17 +5,17 @@ import Image from "next/image";
 
 const WardrobePage = () => {
   return (
-    <div className="min-h-screen w-full bg-white relative overflow-hidden">
+    <div className="min-h-screen w-full bg-gradient-to-r from-[#FFE8DB] to-[#FFE8DB] md:to-white relative overflow-hidden">
       <Navigation />
-      <div className="h-full w-full flex flex-col items-center overflow-auto md:flex-row pt-20 md:pt-[12%] bg-[#FFE8DB]">
+      <div className="min-h-screen w-full flex flex-col items-center gap-2 overflow-auto md:flex-row md:gap-6 pt-20 md:pt-[12%] md:px-6">
         {[1, 2, 3, 4, 5, 6, 7].map((num) => (
           <Image
             className="shrink-0"
             key={num}
             src={`/Wardrobe/${num}.png`}
             alt="wardrobe images"
-            height={600}
-            width={460}
+            height={400}
+            width={380}
           />
         ))}
       </div>

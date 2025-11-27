57573ad75166013ecf576a9a47e9d0300a16fa41
Ashish Singh Negi <ashishamonegi@gmail.com>
2025-11-27 13:13:59 +0530

    fix

--- app/(main)/wardrobe/page.tsx
+++ app/(main)/wardrobe/page.tsx
@@ -7,9 +7,10 @@ const WardrobePage = () => {
   return (
     <div className="min-h-screen w-full bg-white relative overflow-hidden">
       <Navigation />
-      <div className="h-full w-full flex flex-col items-center overflow-auto md:flex md:flex-row pt-20 md:pt-[12%] pb-10 bg-[#FFE8DB]">
+      <div className="h-full w-full flex flex-col items-center overflow-auto md:flex-row pt-20 md:pt-[12%] bg-[#FFE8DB]">
         {[1, 2, 3, 4, 5, 6, 7].map((num) => (
           <Image
+            className="shrink-0"
             key={num}
             src={`/Wardrobe/${num}.png`}
             alt="wardrobe images"
